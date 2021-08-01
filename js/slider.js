2c0ab51520f3d6376d722b87363628ed80be65fd
IlyaDevelop <65752190+IlyaDevelop@users.noreply.github.com>
2021-08-01 20:11:37 +0600

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+$('.slide_content').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true
+});
+
+$('.desktop_vers').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true
+});
